2ae1a487cdca899eb3281ced1a5ecfeaa528d3ce
Hardik9b <76246710+Hardik9b@users.noreply.github.com>
2021-02-21 15:56:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var pig1,pig2
+var box1,box2,box3,box4,box5
+var bird1
+
+var log1,log2,log3,log4
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+
+    box1 = new Box(730,310,70,70);
+    box2 = new Box(890,310,70,70);
+    ground = new Ground(600,height,1200,20);
+    pig1 = new pig(810,310);
+    log1 = new log(810,280,235,PI/2);
+    box3 = new Box(730,190,70,70);
+    box4 = new Box(890,190,70,70);
+    pig2 = new pig(810,190);
+    log2 = new log(810,170,235,PI/2);
+    box5 = new Box(800,100,70,70)
+    log3 = new log(760,40,160,PI/7)
+    log4 = new log(840,40,160,-PI/7)
+    bird1 = new bird(200,100);
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    console.log(box2.body.position.x);
+    console.log(box2.body.position.y);
+    console.log(box2.body.angle);
+    box1.display();
+    box2.display();
+    ground.display();
+    pig1.display();
+    log1.display();
+    box3.display();
+    box4.display();
+    pig2.display();
+    log2.display();
+    box5.display();
+    log3.display();
+    log4.display();
+    bird1.display();
+}
